e16bb2cdef4cc52383a4f5c8736f188b7ede99c7
Harshinipriyaa <94123674+Harshini1112@users.noreply.github.com>
2023-11-18 10:45:47 +0530

    Add files via upload

---
+++ swapnumbers.java
@@ -0,0 +1,20 @@
+
+public class swapnumbers {
+	
+	public static void main(String[] args)
+	{
+		int x,y,t;
+                x = 5;
+                y = 7;
+
+		System.out.println("Before Swapping: " + x " " + y);  
+
+		//swapping 
+
+		t = x;
+		x = y;
+		y = t;
+
+		System.out.println("After swap: " +x " " + y);
+	}
+}
